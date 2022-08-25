a9a846fcc26c723f25b55316fc7a727e774a7ad7
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-08-24 22:59:25 -0300

    :ballot_box_with_check: challenge day done

---
+++ src/components/TukiChild.js
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+const TukiChild = () => {
+    //Nuestro primer hook es el useState, el cual nos permite crear un estado en nuestro componente.
+    // let contador = 0;
+    let animalsArray = ['Carpincho','Yacaré','Tatú','Yaguareté','Guazuncho']
+
+    const [count, setCount] = useState(0);
+    const [loading, setLoading] = useState(false)
+    const [animals, setAnimals] = useState(['Raccoon'])
+    const [info, setInfo] = useState({
+        name: 'Fer',
+        age: 32,
+    })
+    
+    const increase = () => {
+        console.log("Ahora increase funciona")
+        setCount(count + 1)
+    }
+    const decrease = () => {
+        setCount(count -1)
+    }
+    const changeInfo = () => {
+        setInfo({
+            ...info,
+            name: 'Fernando',
+            lastName: 'Clemens',
+        })
+    }
+    const changeLoading = () => {
+        setLoading(!loading)
+    }
+// ### consola: un array de 2 elementos, [Raccoon,['Carpincho','Yacaré','Tatú','Yaguareté','Guazuncho']] ###
+/*     const addAnimals = () => {
+        setAnimals([...animals, animalsArray])
+    } */
+
+// ### consola: un array de 2 elementos, [Raccoon,['Carpincho','Yacaré','Tatú','Yaguareté','Guazuncho']] ###
+    const addAnimals = () => {
+        setAnimals(animals.concat(animalsArray))
+    }
+
+    console.log("TukiChild")
+    console.log("count", count)
+    console.log(info);
+    console.log(loading);
+    console.log(animals);
+    
+    return (
+        <div className="TukiChild">
+            <h1>Tuki Child!</h1>
+            <h2>Soy hijo de TukiComponents y nieto de App</h2>
+            <h3>Contador: {count}</h3>
+            <div>
+                <button className="button" onClick={increase}>Aumentar count!</button>
+                <button className="button" onClick={decrease}>Disminuir count!</button>
+            </div>
+            <div>
+                <button className="button" onClick={changeInfo}>Change Info!</button>
+                <button className="button" onClick={changeLoading}>Change Loading!</button>
+                <button className="button" onClick={addAnimals}>Add Animals!</button>
+            </div>
+        </div>
+    );
+};
+
+export default TukiChild
